dfb554034b8631ca9fccdd9d706ecfc9fbfa2fda
Krakenmeister <johndmeo@gmail.com>
2024-11-07 18:12:55 -0500

    Initial exploration of KMP and upload of drawable resources

--- composeApp/src/commonMain/kotlin/com/krakenmeister/chronoguesser/App.kt
+++ composeApp/src/commonMain/kotlin/com/krakenmeister/chronoguesser/App.kt
@@ -2,7 +2,9 @@ package com.krakenmeister.chronoguesser
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -20,18 +22,11 @@ import chronoguesser.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Welcome to ChronoGuesser!")
         }
     }
 }
